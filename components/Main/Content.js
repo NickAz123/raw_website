0040e86fb6739b5c27a6e00a68f31903ccff9b50
Nick <nick.azurin@gmail.com>
2022-07-07 20:49:01 -0700

    created content component

---
+++ components/Main/Content.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Content.module.css";
+
+function Content() {
+  return <div className={styles.contentContainer}>Main Area</div>;
+}
+
+export default Content;
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,7 +2,16 @@ import React from "react";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
-  return <div className={styles.navbar}>Hello</div>;
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.issueDiv}>
+        <div className={styles.issueText}>S1:I1</div>
+        <div className={styles.issueTitle}>"RAW"</div>
+      </div>
+      <div className={styles.logoDiv}>RAW LOGO</div>
+      <div className={styles.menuDiv}>RAW MENU</div>
+    </div>
+  );
 }
 
 export default Navbar;
