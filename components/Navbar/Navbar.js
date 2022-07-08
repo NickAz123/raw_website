fbdb0c60755c42bd3f79858a98e0c0c9a2f9f661
Nick <nick.azurin@gmail.com>
2022-07-07 19:47:06 -0700

    project initialization

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from ".../.../styles/Navbar.module.css";
+
+function Navbar() {
+  return <div className={styles.navbar}>Hello</div>;
+}
+
+export default Navbar;
