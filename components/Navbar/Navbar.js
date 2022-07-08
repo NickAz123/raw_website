da5c58c83a5e4cd652b7131b6da0e10eabbbe0f8
Nick <nick.azurin@gmail.com>
2022-07-07 20:12:28 -0700

    implemented navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from ".../.../styles/Navbar.module.css";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
   return <div className={styles.navbar}>Hello</div>;
