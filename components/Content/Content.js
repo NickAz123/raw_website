b59cc3614f09ee25ef1d95cf313384bf3a2eabff
Nick <nick.azurin@gmail.com>
2022-07-20 20:13:15 -0700

    added season styling document and added navbar navigation

--- components/Content/Content.js
+++ components/Content/Content.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Content.module.css";
 
-function Content() {
+function Content({ season, issue }) {
   return <div className={styles.contentContainer}></div>;
 }
 
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { useRef, useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import styles from "./Navbar.module.css";
+import useClickOutside from "../Helpers";
 
 function Navbar() {
+  const [navDisplay, setNavDisplay] = useState(false);
+  const ref = useRef(null);
+  useClickOutside(ref, () => setNavDisplay(false));
+
   return (
     <div className={styles.navbar}>
-      <div className={styles.issueDiv}>
-        <div className={styles.issueText}>
-          <span>S1</span>:<span>I1</span>
-        </div>
-        <div className={styles.issueTitle}>"RAW"</div>
+      <div ref={ref} className={styles.issueDiv}>
+        <button
+          className={styles.dropdownButton}
+          onClick={() => setNavDisplay(!navDisplay)}
+        >
+          <span>SEASON 1 - </span>
+          <span>ISSUE 1</span>
+        </button>
+        {navDisplay === true && (
+          <div className={styles.dropdownItems}>
+            <button className={styles.dropdownItemsButton}>Example 1</button>
+            <button className={styles.dropdownItemsButton}>Example 2</button>
+            <button className={styles.dropdownItemsButton}>Example 3</button>
+          </div>
+        )}
       </div>
       <div className={styles.logoDiv}>
         {/* <img src="../public/raw-logo.svg" /> */}
