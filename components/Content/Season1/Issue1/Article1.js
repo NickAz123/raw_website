9ab746334a11d56c391b48fea45dd9ddda21ad79
Nick <nick.azurin@gmail.com>
2022-07-23 17:47:48 -0700

    began first article

--- components/Content/Season1/Issue1/Article1.js
+++ components/Content/Season1/Issue1/Article1.js
@@ -2,7 +2,18 @@ import React from "react";
 import styles from "../../Content.module.css";
 
 function Article1() {
-  return <div className={styles.articleContainer}>This is Article 1</div>;
+  return (
+    <div className={styles.articleContainer}>
+      <div className={styles.articleImgContainer}></div>
+      <div className={styles.articleContent}>
+        <div className={styles.photoCredits}>PHOTO BY KURT SERRANO</div>
+        <div className={styles.articleCredits}>
+          <div className={styles.articleAuthor}>Rico Reyes</div>
+          <div className={styles.articleDate}>JUN-27-2022</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Article1;
