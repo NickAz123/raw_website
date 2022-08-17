4ed4b14b1d5a3be7fa2a457087d36f7f8642f83f
Prince Requino <requino.prince@gmail.com>
2022-08-17 00:02:16 -0400

    finished Article4 / Breakdown of A Breakdown

--- components/Content/Season1/Issue1/Article3.js
+++ components/Content/Season1/Issue1/Article3.js
@@ -25,27 +25,37 @@ function Article3() {
               </b>
             </a>
           </p>
-          <p>I just finished watching The Bear on FX, <i>illegally</i>, and I cannot stress how much of a perfect show this is despite watching the harsh, unfiltered, soul-crushing realities of what could be happening behind the counters of our favorite restaurants. I’m not here to give a review on the show, I’m here to simply just talk about it. </p>
+          <p>I just finished watching The Bear on FX, <i>illegally</i>, and I cannot stress how much of a perfect show this is despite watching the harsh, unfiltered, soul-crushing realities of what could be happening behind the counters of our favorite restaurants.<b> I’m not here to give a review on the show, I’m here to simply just talk about it.</b>Being vulnerable, I’ve re-written this letter twice as well as encountered a minor breakdown only because this industry is my life and I want to be able to fully encapsulate it in the way that I see it. I don’t want to talk about food, I want to talk about the areas in this show that some people would tend to overlook.  </p>
 
           <p>I highly suggest all my friends and family to take the time to watch this show. </p>
 
-          <p><b>**SPOILERS AHEAD**</b></p>
+          <p style={{ color: '#F30B0B' }}><b>TLDR</b></p>
+          <p style={{ color: '#F30B0B' }}><b>**SPOILERS AHEAD**</b></p>
 
-          <p>To provide a better understanding as to what the show’s about, it’s about a young chef, Carmy, leaving the fine-dining scene (best restaurant in the world) and moving back to Chicago to take over his family sandwich shop after his older brother passed away.</p>
+          <p>To provide context, it’s about a young chef, Carmy, leaving the fine-dining scene (best restaurant in the world) and moving back to Chicago to take over his family sandwich shop after his older brother passed away.</p>
 
-          <p>Now this family restaurant is your typical local community joint sustained by untrained cooks coming from different backgrounds all with their own struggles and stories, just like everyone else. It’s the most normal shit ever.</p>
+          <p>Now this family restaurant is your typical local community joint sustained by untrained cooks coming from different backgrounds all with their own struggles and stories, just like everyone else.<i> It’s the most normal shit ever. </i></p>
 
-          <p>As I was watching the series, I caught myself getting distracted or pausing because I couldn’t bear to look at what was and is familiar to me. Whether it was the constant yelling of “behind” or “corner”, drinking water out of liter containers, the importance of family/staff meals, the immense amount of cigarettes, and that one guy that just knows how to fix things. </p>
+          <p>As I was watching, I caught myself getting distracted or pausing because I couldn’t bear to look at what was and is familiar to me. Whether it was the constant yelling of <b>“BEHIND”</b> or <b>“CORNER”</b>, drinking water out of liter containers, the importance of family/staff meals, the immense amount of cigarettes, and that one guy that just knows how to fix things. </p>
 
-          <p>The show highlights the anxieties, addictions, coping mechanisms that a lot of cooks face on a day to day basis and how it’s being managed. In episode 7, you’ll see what I mean. Episode 7 is the perfect description of how a perfectly normal day could immediately turn into a melting pot of utter shit and chaotic distress. </p>
+          <p>The show highlights the anxieties, addictions, coping mechanisms that a lot of cooks face on a day to day basis and how it’s being managed. In episode 7, you’ll see what I mean. Episode 7 is the perfect description of how a perfectly normal day could immediately turn into a <i>melting pot of utter shit and chaotic distress. </i></p>
+
+          <p>There’s a lot to unpack after watching the Bear twice. Once for entertainment, second to study. Just to put this out there, it was kind of the first time I saw myself in a fictional tv character. Off the bat you can really tell that Carmy has not only an eye for food and its quality, but also has an eye for just really nice things. For example, in the beginning you see Carmy dealing with vintage americana. If you really want to dig deep, the restaurant scene has a nice pocket of cooks and chefs with incredible taste. You can see Carmy’s work uniform starting with his Birkenstock Tokio Super Grips paired with not just any white t-shirt, but it’s gotta be either <b>Velva Sheen</b>, <b>Shwanen</b>, or <b>Whitesville</b> noticing that Carmy’s t-shirt had a tubular fit on it. We’re talking $300 in work uniforms. 
+</p>
 
           <p><b>It’s triggering, yet cathartic.</b> Triggering because last summer I wanted k*ll myself, cathartic because it was comforting to know that I’m not alone. </p>
 
-          <p>A lot of my anxiety and depression has been surrounded by work lately and I can’t help but think that there are people out there that earn significantly more than cooks out there by working a desk job. Not to shit on desk jobs, that’s the dream, but we compromise and sacrifice a lot more than other industries would. It’s a high-earning industry, I guess I just don't feel it. <b>It’s like I have a personal vendetta against the industry.</b></p>
+          <p>Triggering because it brought me back to the time I was pulling in at least 55-60 hours weekly, not having control over what bullshit could happen during the day, or how I was meant to be learning but somehow felt the entire restaurant was resting on my shoulders thinking “<i>will this even lead me anywhere? Is this still worth it?</i>” Or moments where I found myself in the walk-in cooler having a breakdown mid-service whilst eating a freezie. I was fresh out of culinary school and was offered a junior-sous chef position at a restaurant that was failing internally. We were probably doing at least 250 covers daily with only 3 or 4 cooks and only 2 hours of prep time. </p>
 
-          <p>But I’m learning to understand that every job has different stresses. I chose this life so I’ve got to ride it out for a bit. I don’t know if that’s a healthy mindset to have, but I’d be lying if I said that my workplace is fun. The Bear helped me sort of rationalize and understand this industry a little bit more and why I’m lucky to be at the restaurant that I’m currently in. I see these guys more than I see my family; <b>the restaurant became my family.</b></p>
+          <p>Comforting because everyone was going through it, everyone was a ticking time-bomb awaiting to break loose. I was at the restaurant more than I was at home, I saw my team more than I saw my family; <b>these cooks became my family.</b> At this point, I felt an intense growth in camaraderie and trust that was built during this season in my life. Comforting because this is a specific experience that I can only share with my peers and superiors who were with me during this season. Comforting because this is an experience only a few can relate to.</p>
 
-          <p>This almost sounds like a selfish newsletter, I’m not asking you to sympathize and feel sad for us. I just sort of want whoever’s reading this to gain an understanding as to what could be happening behind the counter. </p>
+          <p>There’s a lot of yelling, burns, cuts, scars and tattoos which is probably the forefront of what most people would see as cooks or chefs but it’s deeper than that. It’s the cuts and scars that people <b>don’t see</b> — the trauma, the insecurities, the constant state of anxiety and panic that you bear as you sweat your ass off behind the line. There were also a couple of small details in the show that made me go “yup, felt that.” Like when Carmy was drinking water out of a liter container or when he made himself a “whatever’s in my fridge sandwich” and called it a night. Details like this bring me so much comfort because there’s so much more to restaurant life than just cooking. When you watch The Bear, you’re watching an authentic depiction of the literal day in the life of a cook.</p>
+
+          <p>With that said, <b>cooks are cool.</b> But a lot of us are also <b>mentally deranged.</b></p>
+
+          <p>Signing off, </p>
+
+          <p>Rico, Random-Ass Wednesday</p>
         </div>
       </div>
     </div>
