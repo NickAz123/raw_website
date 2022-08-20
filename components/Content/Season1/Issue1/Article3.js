9e57aabc15255455019f29a8522bbadff84af1a7
Nick <nick.azurin@gmail.com>
2022-08-19 23:46:34 -0700

    fixed styling on content img

--- components/Content/Season1/Issue1/Article3.js
+++ components/Content/Season1/Issue1/Article3.js
@@ -5,7 +5,10 @@ function Article3() {
   return (
     <div className={styles.articleContainer}>
       <div className={styles.articleImgContainer}>
-        <img src="/Season1/Issue1/issue_1_article_1_img.png"></img>
+        <img
+          className={styles.img}
+          src="/Season1/Issue1/issue_1_article_1_img.png"
+        ></img>
       </div>
       <div className={styles.articleContent}>
         <div className={styles.photoCredits}>PHOTO BY KURT SERRANO</div>
