1e20b192b08c1fb5ca085a9f9b01fcc8e07fdf8c
Nick <nick.azurin@gmail.com>
2022-08-27 19:09:32 -0700

    fixed a bug where the issue dropdown would not open the issues page when About was open. Refactored function to scroll to top of page when appropriate

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
-  },
+  experimental: {},
 };
 
 module.exports = nextConfig;
