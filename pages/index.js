bea400270227afec06e737b2efc268011cd0e68f
Nick <nick.azurin@gmail.com>
2022-09-08 23:05:28 -0700

    minor refactor for content page state

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import seasonStyles from "../components/Content/Content.module.css";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [currentIssue, setCurrentIssue] = useState(content["Issues"]["Issue1"]);
+  const [currentIssue, setCurrentIssue] = useState(content["Issue1"]);
   const [currentArticle, setCurrentArticle] = useState();
   const [isAboutOpen, setIsAboutOpen] = useState(false);
 
