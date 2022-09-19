13f84468c5c1bbb849db79b148ba85e9924e809e
Nick <nick.azurin@gmail.com>
2022-09-18 21:39:50 -0700

    added article 4 cigarette courtesy

--- components/Content/Issue1/Article4.js
+++ components/Content/Issue1/Article4.js
@@ -8,23 +8,35 @@ function Article4() {
         <img src="/Season1/Issue1/issue_1_article_1_img.png"></img>
       </div>
       <div className={styles.articleContent}>
-        <div className={styles.photoCredits}>PHOTO BY KURT SERRANO</div>
+        <h3>PHOTO BY KURT SERRANO</h3>
         <div className={styles.articleCredits}>
-          <div className={styles.articleAuthor}>Cassidy Fresnoza</div>
-          <div className={styles.articleDate}>AUGUST-15-2022</div>
+          <h4>Cassidy Fresnoza</h4>
+          <h5>SEP-30-2022</h5>
         </div>
-        <div className={styles.articleTitle}>I Hate Orange</div>
+        <h1>I Hate Orange</h1>
         <div className={styles.articleText}>
           <p>
-            <b>HI THERE</b>, My name is Cassidy, but almost everyone that knows
-            me will call me Cass.
+            <a
+              href="https://open.spotify.com/track/4ydabrob4581SkaRuuN9YT"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <b>
+                <i>LISTEN</i>
+              </b>
+            </a>
+          </p>
+          <p>Hi there,</p>
+          <p>
+            My name is Cassidy, but almost everyone that knows me will call me
+            Cass.
           </p>
 
           <p>
             The difference in the way I feel when someone calls me by either
             name is extremely stark. I’ve noticed that they carry a tone of
             their own that somehow shapes the atmosphere between us and what
-            direction our conversation is going to go.{" "}
+            direction our conversation is going to go.
           </p>
 
           <p>
@@ -32,42 +44,27 @@ function Article4() {
             and blink less. While hearing “Cass” on the other hand, it instantly
             connects me to the person. It’s like they’re tapping into a space
             not everyone knows about. I settle into a place of comfort and
-            quietly exhale, then I become more of an open book.{" "}
-          </p>
-
-          <p>
-            Just a couple things to note about me while reading this is that{" "}
-            <span style={{ color: "#FF7A00" }}>
-              {" "}
-              I ABSOLUTELY HATE THE COLOR ORANGE
-            </span>
-            . Also, that I put lots of weight on the environment I constantly
-            find myself in (which might explain why I love hearing my nickname
-            so much more), not only because of what it looks and feels like, but
-            because of the effects it has on me and those around me.{" "}
+            quietly exhale, then I become more of an open book.
           </p>
 
           <p>
             To be honest with you, writing this was a struggle for me in the
             beginning. But today I am sitting outside under trees with the
             breeze grazing my face, all while sipping on a matcha latte (my
-            fave). How could one possibly not get inspired? It’s crazy to me how
-            quick a fresh atmosphere can change you. It’s such a beautiful
-            feeling that is able to produce some of the healthiest human
-            responses.{" "}
-            <u>
-              This very feeling happens to be one of the reasons I took up
-              Interior Design.
-            </u>
+            fave). It’s crazy to me how quick a fresh atmosphere can change you.
+            It’s such a beautiful feeling that is able to produce some of the
+            healthiest human responses. This very feeling happens to be one of
+            the reasons I took up Interior Design.
           </p>
 
           <p>
-            To put it plainly, the lens that I see through has always been
-            curious. Constantly craving inspiration out of mundane circumstances
-            and always asking questions like “what could I possibly create out
-            of this?” or “ew, how can I get myself out of this situation?”.
-            While possibly, simultaneously feel bouts of “this makes me cringe,
-            and I NEED to change it.”{" "}
+            Just a couple things to note about me while reading this is that I
+            absolutely{" "}
+            <span style={{ color: "#FF7A00" }}> hate the color orange.</span>{" "}
+            Also, that I put lots of weight on the environment I constantly find
+            myself in (which might explain why I love hearing my nickname so
+            much more), not only because of what it looks and feels like, but
+            because of the effects it has on me and those around me.
           </p>
 
           <p>
@@ -77,46 +74,46 @@ function Article4() {
             start to become more irritated? You all of the sudden get
             uncomfortable and want to change your environment. You start to get
             annoyed and start to look for shade and a cooler place to go to.
-            Have you ever felt this? Like ever? If your answer is yes, I feel
-            you. And if your answer is a no, then this means you like being put
-            in uncomfortable situations and you’re a weirdo, or maybe you’re
-            just patient. Ever since I could remember, the correlation between
-            one’s response and their tangible environment has been a consistent
-            observation of mine.{" "}
+            Have you ever felt this? If your answer is yes, I feel you. And if
+            your answer is a no, then this means you like being put in
+            uncomfortable situations, potentially a weirdo, or maybe you’re just
+            patient. Ever since I could remember, the correlation between one’s
+            response and their tangible environment has been a consistent
+            observation of mine.
           </p>
 
           <p>
             Many may think about pretty picture frames or rugs upon hearing
             “Interior Design”, but let me tell you, It is WAY more than that.
             One thing I hope to clear up while you read this is the extreme
-            contrast between what Interior Decoration and Interior Designing
+            contrast between what <i>Interior Decoration</i> and{" "}
+            <i>Interior Designing </i>
             really is. A definition that I came across from the NCIDQ (The
-            Council for Interior Design Qualification) put it perfectly.{" "}
+            Council for Interior Design Qualification) put it perfectly.
           </p>
 
-          <div className={styles.articleQuote}>
-            <p className={styles.pMargin}>
-              <b>
-                “Interior design is the art and science of understanding
-                people’s behaviour to create functional spaces within a
-                building. The decoration is the furnishing or adorning of a
-                space with fashionable or beautiful things*. In short, interior
-                designers may decorate, but decorators do not design”.
-              </b>
-            </p>
-          </div>
+          <p className="quote">
+            “Interior design is the art and science of understanding people’s
+            behavior to create functional spaces within a building. The
+            decoration is the furnishing or adorning of a space with fashionable
+            or beautiful things*. In short, interior designers may decorate, but
+            decorators do not design”.
+          </p>
 
           <p>
-            When I tell you my brain blew up when I first read that,{" "}
-            <b>it blew. up.</b> I was never able to articulate the right words
-            to capture how different they were from each other. I began to think
-            that these differences were things that I only saw myself. I began
-            to believe that maybe I was too opinionated and too stubborn to want
-            to admit that maybe they were just the same. It made me realize that
-            your experiences and thoughts can become valid through only one
-            encounter with something that makes sense to you. TBH, in a sense,
-            that’s also what interior design is to me. An encounter, or even
-            multiple encounters with a space that <b>makes sense to YOU. </b>
+            When I tell you my brain blew up when I first read that, it blew.
+            up. I was never able to articulate the right words to capture how
+            different they were from each other. I began to think that these
+            differences were things that I only saw myself. I began to believe
+            that maybe I was too opinionated and too stubborn to want to admit
+            that maybe they were just the same. It made me realize that your
+            experiences and thoughts can become valid through only one encounter
+            with something that makes sense to you. TBH, in a sense, that’s also
+            what interior design is to me.{" "}
+            <b>
+              An encounter, or even multiple encounters with a space that makes
+              sense to YOU.
+            </b>
           </p>
 
           <p>
@@ -128,7 +125,25 @@ function Article4() {
             space could just be a reflection of who they are and who (and what)
             they love. Whether or not they prefer the designs from Living Spaces
             over Eames is just a bi-product of how they choose to express
-            themselves from the inside out.{" "}
+            themselves from the inside out.
+          </p>
+
+          <p>
+            I could ramble on and on about how much I love the concept of
+            designing as a whole but let me just leave you with something I’ve
+            learned overtime;
+          </p>
+
+          <p>
+            Don’t be afraid to create a space that is meaningful to you. Clean
+            your kitchen, play your favorite songs (or just leave it quiet),
+            rearrange your room or go for a nice walk, it’s all up to you. These
+            tasks are so fun and bring waves of refreshment, but the real weight
+            of creating space for a life that is meaningful to you all comes
+            down to your very own consumption; who and what you listen to,
+            surround yourself with, and invest in. Be intentional with the
+            things you let in your home and in your mind. It all starts from
+            there.
           </p>
 
           <p>
@@ -150,20 +165,18 @@ function Article4() {
           </p>
 
           <p>
-            Again, I could keep going, but to be completely honest with you, my
-            eyeballs are burning and drying out from looking at my laptop
-            screen. Also, I just don’t want to bore you with any more words even
-            if this probably isn’t a lot of reading for lots of people. I hope
-            this doesn’t end here though. I love and value conversing with
-            others to the point that most days, that’s all I really do. So if
-            you’re reading this, thank you so much. If you need design advice,
-            another mind to merge with, or just a friend, you now have me.{" "}
+            Again, I could keep going and I hope this doesn’t end here. I love
+            and value conversing with others to the point that most days, that’s
+            all I really do. So if you’re reading this, thank you so much. If
+            you need design advice, another mind to merge with, or just a
+            friend, you now have me.
           </p>
 
           <p>
-            Oh and one more thing, if you actually like the color orange, we
-            absolutely NEED to have a conversation. But also please don’t
-            forget, you can just call me Cass.{" "}
+            Oh and one more thing, if you actually like the color{" "}
+            <span style={{ color: "#FF7A00" }}>orange</span>, we absolutely NEED
+            to have a conversation. But also please don’t forget,{" "}
+            <b>you can just call me Cass.</b>
           </p>
 
           <p>With love,</p>
